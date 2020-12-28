b778306bc713d7c4c1a70fc8620bf4b1e8c92547
Vladislav Prekel <misterptits@yandex.ru>
2020-12-29 00:38:27 +0700

    not working

--- src/App.fs
+++ src/App.fs
@@ -2,13 +2,14 @@ module App
 
 open Browser.Dom
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
+let links =
+    document.getElementById ("links") :?> Browser.Types.HTMLDivElement
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+let input =
+    document.getElementById ("search_form_input") :?> Browser.Types.HTMLInputElement
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+let v = input.value
+
+printfn "%s12" v
+
+links.setAttribute ("style", "width: 700px")
