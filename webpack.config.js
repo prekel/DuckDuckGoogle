72b731e7c8f669830da9ac7a389be098ce7d9dcc
Vladislav Prekel <misterptits@yandex.ru>
2020-12-29 01:58:01 +0700

    улучшено

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ var path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./src/App.fsproj",
+    entry: "./src/DuckDuckGoogle.fsproj",
     output: {
         path: path.join(__dirname, "./dist"),
         filename: "bundle.js",
     },
+    devtool: "eval-source-map",
     module: {
         rules: [{
             test: /\.fs(x|proj)?$/,
-            use: "fable-loader"
+            use: {
+                loader: "fable-loader",
+                options: {
+                    define: ["DEBUG"]
+                }
+            }
         }]
     }
 }
