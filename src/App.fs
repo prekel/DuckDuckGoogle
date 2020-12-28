2209972547fa932a6a8f8c2ef70cad00e0629c7c
Vladislav Prekel <misterptits@yandex.ru>
2020-12-29 01:07:30 +0700

    -/asdasd

--- src/App.fs
+++ src/App.fs
@@ -2,14 +2,13 @@ module App
 
 open Browser.Dom
 
-let private links =
-    document.getElementById ("links") :?> Browser.Types.HTMLDivElement
+// Mutable variable to count the number of times we clicked the button
+let mutable count = 0
 
-let private input =
-    document.getElementById ("search_form_input") :?> Browser.Types.HTMLInputElement
+// Get a reference to our button and cast the Element to an HTMLButtonElement
+let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
 
-let private v = input.value
-
-printfn "%s12" v
-
-links.setAttribute ("style", "width: 700px")
+// Register our listener
+myButton.onclick <- fun _ ->
+    count <- count + 1
+    myButton.innerText <- sprintf "You clicked: %i time(s)" count
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/DuckDuckGoogle.fsproj",
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [{
+            test: /\.fs(x|proj)?$/,
+            use: "fable-loader"
+        }]
+    }
+}
